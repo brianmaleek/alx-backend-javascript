7cc9be6fa4c8d6cf94943aa3aa2b2cc748e23045
washucode <estherw8525@gmail.com>
2023-08-10 19:33:29 +0300

    add amenity model

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a class Amenity"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """defines a Amenity that inherits from BaseModel."""
+    name = ""
