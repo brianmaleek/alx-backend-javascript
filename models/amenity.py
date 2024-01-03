48e850d7f176a40e24db3ff2ecfaab43d53df5dc
Brian Kin Maleek <maleekmalik@gmail.com>
2024-01-03 19:46:44 +0300

    Delete models directory
    Messed Repo

--- models/amenity.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""This module defines a class Amenity"""
-
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """defines a Amenity that inherits from BaseModel."""
-    name = ""
