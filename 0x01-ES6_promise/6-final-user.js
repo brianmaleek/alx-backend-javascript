91cf0a0e6dba8e7087a3a156852961ab58b9b792
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-01-04 03:05:15 +0300

    6-final-user.js fixed linting

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,5 @@
-/* eslint-disable import/extensions */
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName = '', lastName = '', filename = '') {
   return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)]);
