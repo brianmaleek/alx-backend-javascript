7fa1d9bd05d4375416f5ebd680e29f0f80ab823a
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-01-04 03:02:15 +0300

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,25 +2,6 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  try {
-    const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
-
-    return [
-      {
-        status: userResult.status,
-        value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason,
-      },
-      {
-        status: photoResult.status,
-        value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason,
-      },
-    ];
-  } catch (error) {
-    console.error('Error during signup:', error);
-    return [];
-  }
+export default function handleProfileSignup(firstName = '', lastName = '', filename = '') {
+  return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)]);
 }
