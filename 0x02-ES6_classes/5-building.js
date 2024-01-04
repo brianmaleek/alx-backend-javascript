75fb995fc705b5a560af83ee199dd8cd4438dda3
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-01-05 02:08:56 +0300

    File: 5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,29 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = this.validateNumber(sqft, 'sqft');
+  }
+
+  // Getters and setters for sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = this.validateNumber(value, 'sqft');
+  }
+
+  // Abstract method - must be implemented by subclasses
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+
+  // Validate number
+  // eslint-disable-next-line class-methods-use-this
+  validateNumber(value, name) {
+    if (typeof value !== 'number' || Number.isNaN(value)) {
+      throw new TypeError(`${name} must be a number`);
+    }
+    return value;
+  }
+}
