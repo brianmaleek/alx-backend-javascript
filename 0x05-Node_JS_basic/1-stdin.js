e99a674bf9ad0797cdfae55e13f11aeba70fd74f
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-02-28 03:49:33 +0300

    1-stdin.js -- Using Process stdin -- intranet checker 2 fix -- 5

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,15 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Create an event listener for stdin data
-process.stdin.on('data', (data) => {
-  const inputData = data.toString().trim();
-
-  // Display the input back to the user
-  process.stdout.write(`Your name is: ${inputData}\n`);
-  process.exit();
+process.stdin.on('readable', () => {
+  const inputData = process.stdin.read();
+  if (inputData !== null) {
+    // Display the input back to the user
+    process.stdout.write(`Your name is: ${inputData.toString().trim()}\n`);
+  }
 });
 
 // If stdin is not connected to a terminal, handle process exit event
-if (!process.stdin.isTTY) {
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
